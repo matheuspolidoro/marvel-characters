8f11c9ef3a69e7e06d29988aea5a6ecd0190d517
matheuspolidoro <matheuspolidoro@outlook.com>
2022-01-10 03:09:16 -0300

    feat: finish detail page

--- src/app/pages/character-detail/character-detail.component.ts
+++ src/app/pages/character-detail/character-detail.component.ts
@@ -1,5 +1,6 @@
-import { Observable } from 'rxjs';
-import { Router } from '@angular/router';
+import { ApiMarvelCharacterService } from 'src/app/services/api-marvel-character.service';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,15 +9,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./character-detail.component.scss'],
 })
 export class CharacterDetailComponent implements OnInit {
-  constructor(public router: Router) {
-    console.log(
-      '%c%s',
-      'color: #f279ca',
-      this.router.getCurrentNavigation()?.extras.state?.character.id
-    );
+  constructor(
+    public router: Router,
+    private apiService: ApiMarvelCharacterService,
+    private route: ActivatedRoute
+  ) {
+    if (this.router.getCurrentNavigation()?.extras.state?.character) {
+      this.dataFromNavigation =
+        this.router.getCurrentNavigation()?.extras.state?.character;
+    }
   }
 
-  state$: Observable<object>;
+  dataFromNavigation: any = null;
+  character$: BehaviorSubject<any> = new BehaviorSubject<any>(undefined);
+  subscription$ = new Subscription();
+  characterNotFound: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
+    false
+  );
+  imageSource: any;
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (this.dataFromNavigation) {
+      this.character$.next(this.dataFromNavigation);
+      this.imageSource =
+        this.dataFromNavigation.thumbnail.path +
+        '.' +
+        this.dataFromNavigation.thumbnail.extension;
+    } else {
+      this.subscription$ = this.route.params.subscribe((params) => {
+        if (params?.characterId) {
+          this.apiService
+            .getCharacterDetail(params.characterId)
+            .subscribe((apiRes) => {
+              this.character$.next(apiRes.data.results[0]);
+              this.imageSource =
+                apiRes.data.results[0].thumbnail.path +
+                '.' +
+                apiRes.data.results[0].thumbnail.extension;
+            }),
+            () => this.characterNotFound.next(true);
+        } else {
+          this.router.navigate(['../../1'], { relativeTo: this.route });
+        }
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscription$.unsubscribe();
+  }
 }
